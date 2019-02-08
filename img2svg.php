db4109e02be93c0d553d3e4b4b77a008190e8c97
Struan Clark <github@xtruan.com>
2019-02-08 15:42:29 -0700

    changing microtime to time

--- img2svg.php
+++ img2svg.php
@@ -1,6 +1,6 @@
 <?php
 $target_dir = "uploads/";
-$target_file = $target_dir . microtime() . "_" . basename($_FILES["fileToUpload"]["name"]);
+$target_file = $target_dir . time() . "_" . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 // Check if image file is a actual image or fake image
